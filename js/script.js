749a9ccb41c3648df842ca4038b79feab70b4a98
ajjoyal01 <ajjoyal01@users.noreply.github.com>
2024-10-09 12:35:23 -0400

    header and nav style changes

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// window.onscroll = function() {
+//     var nav = document.getElementById('navbar');
+//     if ( window.scrollY > 0 ) {
+//         //nav.classList.add("navbar-opaque");
+//         console.log(Math.max(window.scrollY / 30, 1));
+//         var color = "rgba(94, 45, 117, " + Math.min(window.scrollY / 30, 1) + ")"
+//         nav.style.backgroundColor = color;
+//     } else {
+//         nav.style.backgroundColor = 'rgba(94, 45, 117, 0)';
+//     }
+// }
